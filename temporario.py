34dfff4ed6e1ae84d1900ce7283f4ea7152b1c99
treicy600 <treicy.0932@aluno.pr.senac.br>
2025-02-26 20:06:53 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
 st.header("Cabeçalho")
+
+st.multiselect(
+  'Quais são suas cores favoritas?',
+  ['verde', 'amarelo', 'vermelho', 'azul'],
+  ['amarelo', 'vermelho'])
+
 st.toggle("toggle")
 st.text_area("Enter text")
-st.text_imput(" ")
+st.text_input("")
 st.button("Botão salvar")
 st.selectbox(
   'Qual a sua cor favorita?',

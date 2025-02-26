65bef6fc0f3174acf43684b3afc959582b13dd54
treicy600 <treicy.0932@aluno.pr.senac.br>
2025-02-26 20:15:50 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -14,4 +14,11 @@ st.button("Botão salvar")
 st.selectbox(
   'Qual a sua cor favorita?',
   ('Azul', 'Vermelho', 'verde'))
+
+st.checkbox('Sorvete')
+st.checkbox('café')
+st.checkbox('Refrigerante')
+
+options = ["North", "East", "south", "west"]
+selection = st.pills("Directions", options, selection_mode="multi")
 st.button("Botão Salvar")

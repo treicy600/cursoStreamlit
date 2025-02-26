d008ff37af4e26e342d7ddb9eb59dc4873bc99a7
treicy600 <treicy.0932@aluno.pr.senac.br>
2025-02-26 19:58:46 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -5,3 +5,7 @@ st.toggle("toggle")
 st.text_area("Enter text")
 st.text_imput("")
 st.button("Botão salvar")
+st.selectbox(
+  'Qual a sua cor favorita?',
+  ('Azul', 'Vermelho', 'verde'))
+st.button("Botão Salvar")

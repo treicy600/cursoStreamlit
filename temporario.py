96e05252417d31307abb08da20e3d1581c7c911a
treicy600 <treicy.0932@aluno.pr.senac.br>
2025-02-26 19:59:23 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header("Cabeçalho")
 st.toggle("toggle")
 st.text_area("Enter text")
-st.text_imput("")
+st.text_imput(" ")
 st.button("Botão salvar")
 st.selectbox(
   'Qual a sua cor favorita?',
